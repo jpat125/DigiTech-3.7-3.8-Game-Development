dae01bd0ce2e18f3fe13503ee59b892a67b551a3
Jaiden Patel <jaidenpat380@gmail.com>
2023-05-18 18:12:36 +1200

    Added in 90 degree turn in corridor
    Added collision shape to vertical wall of existing corridor and then copy pasted and turned 90 degrees to form a right hand turn in the corridor, might need to add in additional collision shape as capsule currently able to fall through the middle.

--- CharacterBody3Dscript.gd
+++ CharacterBody3Dscript.gd
@@ -9,16 +9,16 @@ var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var neck := $Neck
 @onready var camera := $Neck/Camera3d
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	elif event.is_action_pressed("ui_cancel"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		if event is InputEventMouseMotion:
-			neck.rotate_y(-event.relative.x * 0.01)
-			camera.rotate_x(-event.relative.y * 0.01)
-			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
+#func _unhandled_input(event: InputEvent) -> void:
+#	if event is InputEventMouseButton:
+#		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+#	elif event.is_action_pressed("ui_cancel"):
+#		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+#	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+#		if event is InputEventMouseMotion:
+#			neck.rotate_y(-event.relative.x * 0.00)
+#			camera.rotate_x(-event.relative.y * 0.00)
+#			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(0), deg_to_rad(0))
 
 
 func _physics_process(delta: float) -> void:
