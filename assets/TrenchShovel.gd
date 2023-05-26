a51034c6338fa4078812f115833c913ef83976bc
Jaiden Patel <jaidenpat380@gmail.com>
2023-05-26 15:06:55 +1200

    Working on shovel
    Need to set up signal.

---
+++ assets/TrenchShovel.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_meta("Shovel", 1)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_area_entered(Area3D):
+	if Area3D.name == "capsule_hitbox_a3d": 
+		queue_free()
