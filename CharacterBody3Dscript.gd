559d01039979be9c8543440ff5fb3e5422a4ee36
Jaiden Patel <jaidenpat380@gmail.com>
2023-06-01 10:15:44 +1200

    Got shovel collison working
    Needed to set Area3d as root node. Next step - make wall only break if capsule has shovel.

--- CharacterBody3Dscript.gd
+++ CharacterBody3Dscript.gd
@@ -44,3 +44,4 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 	
+
--- shovelscript.gd
+++ shovelscript.gd
@@ -1,9 +1,9 @@
-extends Area3D
+extends Node3D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_meta("Patch", 1)
+	set_meta("Shovel", 1)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,5 +11,11 @@ func _process(delta):
 	pass
 
 func _on_area_entered(area):
+	print("sure")
 	if area.name == "capsule_hitbox_a3d": 
 		queue_free()
+		
+func _globl_shovel_function():	
+	var shovel_global
+	# trying to make wall only break if capsule has picked up shovel.
+
