c02dbd74dfd1d3819201e576118059fdea794d04
Jaiden Patel <jaidenpat380@gmail.com>
2023-06-08 10:15:49 +1200

    Working on conditional collision
    Got Nick to help, changed movement code to stuff from Godot docs, but is out of date so will need to talk to RBS.

--- shovelscript.gd
+++ shovelscript.gd
@@ -4,16 +4,14 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_meta("Shovel", 1)
-	var player = get_tree().get_root().get_node("Node3D/CharacterBody3D")
-	player.set_meta("has_shovel", 1)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func _on_area_entered(area):
-	print("sure")
-	if area.name == "capsule_hitbox_a3d":
-		queue_free()
+#func _on_area_entered(area):
+#	print("sure")
+#	if area.name == "capsule_hitbox_a3d":
+##		queue_free()
 
