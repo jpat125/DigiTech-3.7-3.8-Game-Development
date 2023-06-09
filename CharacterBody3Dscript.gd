ed129935e6cd0f5f8018b512eaf8e78354673750
Jaiden Patel <jaidenpat380@gmail.com>
2023-06-09 16:30:34 +1200

    Reducing number of errors
    Have emailed RBS, awaiting help.

--- CharacterBody3Dscript.gd
+++ CharacterBody3Dscript.gd
@@ -2,15 +2,13 @@ extends CharacterBody3D
 #var has_shovel = false
 func _ready():
 	set_meta("CharacterBody3D", 1)
-
-# How fast the player moves in meters per second.
+	
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 # Get the gravity from the project settings to be synced with RigidDynamicBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var neck := $Neck
-@onready var camera := $Neck/Camera3D
-
+@onready var camera := $Neck/Camera3d
 #func _unhandled_input(event: InputEvent) -> void:
 #	if event is InputEventMouseButton:
 #		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -18,29 +16,23 @@ var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 #		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 #	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 #		if event is InputEventMouseMotion:
-#			neck.rotate_y(-event.relative.x * 0.01)
-#			camera.rotate_x(-event.relative.y * 0.01)
-#			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
-
-
+#			neck.rotate_y(-event.relative.x * 0.00)
+#			camera.rotate_x(-event.relative.y * 0.00)
+#			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(0), deg_to_rad(0))
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "back")
-	var direction = (neck.transform.basis.rotated(Vector3.UP, rotation.y) * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
