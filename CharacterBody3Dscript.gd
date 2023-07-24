1a8d98776608272e1295ef4da7e6846a1f880108
Jaiden Patel <jaidenpat380@gmail.com>
2023-07-24 15:08:27 +1200

    Reducing jump easter egg

--- CharacterBody3Dscript.gd
+++ CharacterBody3Dscript.gd
@@ -5,7 +5,7 @@ func _ready():
 	set_meta("CharacterBody3D", 1)
 	
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const JUMP_VELOCITY = 3
 # Get the gravity from the project settings to be synced with RigidDynamicBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var neck = $Neck
