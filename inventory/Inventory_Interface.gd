623c40fd2b26a61b1efe55e4c96bc75d609fa818
Jaiden Patel <jaidenpat380@gmail.com>
2023-08-04 13:30:24 +1200

    Inventory working, fixed bug
    [30:00 (need to do var)]
    
    Fixed bug on own, up to making 'moving items'.

--- inventory/Inventory_Interface.gd
+++ inventory/Inventory_Interface.gd
@@ -1,7 +1,31 @@
 extends Control
 
+var grabbed_slot_data: SlotData
+
 @onready var player_inventory: PanelContainer = $PlayerInventory
-#@onready var inventory_interface: Control = $UI/InventoryInterface #trying to get 2ui from it's own node
+@onready var grabbed_slot: PanelContainer = $GrabbedSlot
+
+
+
+
 
 func set_player_inventory_data(inventory_data: InventoryData) -> void:
+	inventory_data.inventory_interact.connect(on_inventory_interact)
 	player_inventory.set_inventory_data(inventory_data)
+
+func on_inventory_interact(inventory_data: InventoryData, 
+		index: int, button: int) -> void:
+
+	match [grabbed_slot_data, button]:
+		[null, MOUSE_BUTTON_LEFT]:
+			grabbed_slot_data=inventory_data.grab_slot_data(index)
+	print (grabbed_slot_data)
+	
+	update_grabbed_slot()
+	
+func update_grabbed_slot() -> void:
+	if grabbed_slot_data:
+		grabbed_slot.show()
+		grabbed_slot.set_slot_data(grabbed_slot_data)
+	else:
+		grabbed_slot.hide()
--- inventory/slot.gd
+++ inventory/slot.gd
@@ -18,7 +18,7 @@ func set_slot_data(slot_data: SlotData) -> void:
 
 func _on_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton \
-				and (event.button_index == MOUSE_BUTTON_LEFT) \
-				or event.button_index == MOUSE_BUTTON_RIGHT \
+				and (event.button_index == MOUSE_BUTTON_LEFT \
+				or event.button_index == MOUSE_BUTTON_RIGHT) \
 				and event.is_pressed():
 			slot_clicked.emit(get_index(), event.button_index)
