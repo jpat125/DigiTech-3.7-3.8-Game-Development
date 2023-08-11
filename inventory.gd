70d7ef7b2fd09eb1ef7887aef726ba27f7f29c3d
Jaiden Patel <jaidenpat380@gmail.com>
2023-08-11 14:07:45 +1200

    Still fixing issue
    Nightmare Nightmare Nightmare.
    
    Also started rigging character....weight painting is a PITA...

--- inventory.gd
+++ inventory.gd
@@ -3,7 +3,7 @@ extends PanelContainer
 const Slot = preload("res://item_slot.tscn")
 
 @onready var item_grid: GridContainer = $MarginContainer/ItemGrid
-		
+
 func set_inventory_data(inventory_data: InventoryData) -> void:
 	inventory_data.inventory_updated.connect(populate_item_grid)
 	populate_item_grid(inventory_data)
--- inventory/inventory_data.gd
+++ inventory/inventory_data.gd
@@ -2,7 +2,6 @@ extends Resource
 class_name InventoryData
 
 signal inventory_updated(inventory_data: InventoryData)
-
 signal inventory_interact(inventory_data: InventoryData, index: int, button: int)
 
 @export var slot_datas: Array[SlotData]
@@ -21,13 +20,13 @@ func grab_slot_data(index: int) -> SlotData:
 
 func drop_slot_data(grabbed_slot_data: SlotData, index: int) -> SlotData:
 	var slot_data = slot_datas[index]
-	
-	slot_datas[index] = grabbed_slot_data
-	inventory_updated.emit(self)
-		if slot_data:
+
+	if slot_data:
+			slot_datas[index] = null
+			inventory_updated.emit(self)
 			return slot_data
 	else:
-		return null
+			return null
 
 
 
