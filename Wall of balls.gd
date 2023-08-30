2a406b657087a153fff72586cd9188c47a72c9dd
Jaiden Patel <jaidenpat380@gmail.com>
2023-08-30 12:31:06 +1200

    Working on screenshots for DCAT
    Trying to fix height of model and adding in other stuff.

--- Wall of balls.gd
+++ Wall of balls.gd
@@ -11,5 +11,5 @@ func _process(delta):
 	pass
 
 func _on_area_entered(StaticBody3D):
-	if StaticBody3D.name == "capsule_hitbox_a3d":
+	if StaticBody3D.name == "capsule_hitbox_a3d" and BootlegGlobalVariable._shovel_status == 1:
 		queue_free()
