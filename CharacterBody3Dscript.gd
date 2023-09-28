c132f08c4b6a95633a3b45eabe4b18081d9ad158
Jaiden Patel <jaidenpat380@gmail.com>
2023-09-28 14:47:18 +1300

    Trying to fix audio
    Spend much longer than normal, due to starting day with work, rather than starting it halfway through. I managed to make good progress on the audio system, before it broke again. Used .ogg files instead of .wav as it is easier to loop. Issue is that now the buzzing noise is playing instead of the actual sfx, this could be due to Godot rapidly trying to play the sfx over and over. Will investigate and hopefully come back tonight and ultimately put this issue to bed.

--- CharacterBody3Dscript.gd
+++ CharacterBody3Dscript.gd
@@ -27,6 +27,9 @@ var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 #			neck.rotate_y(-event.relative.x * 0.00)
 #			camera.rotate_x(-event.relative.y * 0.00)
 #			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(0), deg_to_rad(0))
+
+
+	
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -43,21 +46,30 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "back")
-	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-		$soldier/AnimationPlayer.play("forward_walk")
-		$walking.play()
-	else:
-		#idle movements and sounds
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-		$soldier/AnimationPlayer.play("idle")
-		$idle_breathing.play()
 	
+	#if velocity.x > 0 and velocity.z > 0:
+	#	$walking.play()
+		
+	#else:
+	#	$idle_breathing.play()
+		
+	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if direction:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+			$soldier/AnimationPlayer.play("forward_walk")
+			$walking.play()
+			$idle_breathing.stop()
+	else:
+			#idle movements and sounds
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
+			$soldier/AnimationPlayer.play("idle")
+			$walking.stop()
+			$idle_breathing.play()
+
 	move_and_slide()
 
 func interact()-> void:
